508449967f21274e8cb57c86e3e64d35545f1dbd
Nick Gerrard <nick.gerrard16@gmail.com>
2026-05-08 08:01:30 -0400

    fix merge issue

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -3,6 +3,19 @@ return {
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
+<<<<<<< HEAD
+    local harpoon = require("harpoon")
+    harpoon:setup()
+
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end,          { desc = "Harpoon add file" })
+    vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon menu" })
+
+    vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "Harpoon file 1" })
+    vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "Harpoon file 2" })
+    vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "Harpoon file 3" })
+    vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "Harpoon file 4" })
+  end,
+=======
     require("harpoon"):setup()
   end,
   keys = {
@@ -50,4 +63,5 @@ return {
       desc = "Harpoon file 4",
     },
   },
+>>>>>>> 102de39451e912b71349a599a3f23eb3c6a8dbee
 }
